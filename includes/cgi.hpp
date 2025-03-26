3d2a0df5ab1e6d1f2839f5f02d449d9593b25968
adjoly <adamjly@proton.me>
2025-03-26 20:49:02 +0100

    「🏗️」 wip: Forgot to add protocol into constructor

--- includes/cgi.hpp
+++ includes/cgi.hpp
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 15:13:56 by adjoly            #+#    #+#             */
-/*   Updated: 2025/03/26 16:48:32 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/03/26 20:46:55 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,22 @@ private:
   std::string &_cgi_exec_path; ///> The path of the cgi executable
   std::string &_cgi_file_path; ///> The path of the file to be executed
   std::string &_http_methods;  ///> The http method of the request
+  std::string &_http_protocol; ///> The http protocol and it's version
   std::map<std::string, std::string>
       &_http_header; ///> A map of the header of the http request
 
   std::vector<std::string>
-      __cgi_envp; ///> The envp that will be used to exec the cgi
+      _cgi_envp; ///> The envp that will be used to exec the cgi
   std::vector<std::string>
       _cgi_argv; ///> The argv that will be used to exec the cgi
-				 
+
   /// more internal function will be added but none of your business :D
   /// only shit will be here
+
+  /**
+   *	@brief	Can be used to get the correct env for executing the CGI
+   */
+  void _getCgiEnvp(void);
 };
 
 }; // namespace webserv
