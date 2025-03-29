8907593dc728d47a652ac354712a9216cdfffc45
adjoly <adamjly@proton.me>
2025-03-29 17:56:00 +0100

    「🏗️」 wip: envp for cgi will be working one day i promise

--- includes/cgi.hpp
+++ includes/cgi.hpp
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 15:13:56 by adjoly            #+#    #+#             */
-/*   Updated: 2025/03/26 20:46:55 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/03/29 14:17:33 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,48 +19,49 @@
 namespace webserv {
 
 class Cgi {
-public:
-  /**
-   *	@brief	Contructor that can be used to execute a cgi file
-   *
-   *	@param	The path of the cgi program
-   *	@param	The location where the script will be executed
-   *	@param	The name of cgi script that will be executed
-   *	@param	A map of the http header
-   *	@param	A string of the http method used
-   */
-  Cgi(std::string &, std::string &, std::string &, std::map<std::string, std::string> &,
-      std::string &);
-  ~Cgi(void);
+  public:
+	/**
+	 *	@brief	Contructor that can be used to execute a cgi file
+	 *
+	 *	@param	The path of the cgi program
+	 *	@param	The location where the script will be executed
+	 *	@param	The name of cgi script that will be executed
+	 *	@param	A map of the http header
+	 *	@param	A string of the http method used
+	 */
+	Cgi(std::string &, std::string &, std::string &,
+		std::map<std::string, std::string> &, std::string &);
+	~Cgi(void);
 
-  /**
-   *	@brief	Can be used to get the response of the cgi
-   *
-   *	@return	The http response of the cgi
-   */
-  std::string getResponse(void);
+	/**
+	 *	@brief	Can be used to get the response of the cgi
+	 *
+	 *	@return	The http response of the cgi
+	 */
+	std::string getResponse(void);
 
-protected:
-private:
-  std::string &_cgi_exec_path; ///> The path of the cgi executable
-  std::string &_cgi_file_path; ///> The path of the file to be executed
-  std::string &_http_methods;  ///> The http method of the request
-  std::string &_http_protocol; ///> The http protocol and it's version
-  std::map<std::string, std::string>
-      &_http_header; ///> A map of the header of the http request
+  protected:
+  private:
+	std::string &_cgi_exec_path; ///> The path of the cgi executable
+	std::string &_cgi_file_path; ///> The path of the file to be executed
+	std::string &_http_methods;	 ///> The http method of the request
+	std::string &_http_protocol; ///> The http protocol and it's version
+	std::string &_server_port; ///> The port of the server
+	std::map<std::string, std::string>
+		&_http_header; ///> A map of the header of the http request
 
-  std::vector<std::string>
-      _cgi_envp; ///> The envp that will be used to exec the cgi
-  std::vector<std::string>
-      _cgi_argv; ///> The argv that will be used to exec the cgi
+	std::vector<std::string>
+		_cgi_envp; ///> The envp that will be used to exec the cgi
+	std::vector<std::string>
+		_cgi_argv; ///> The argv that will be used to exec the cgi
 
-  /// more internal function will be added but none of your business :D
-  /// only shit will be here
+	/// more internal function will be added but none of your business :D
+	/// only shit will be here
 
-  /**
-   *	@brief	Can be used to get the correct env for executing the CGI
-   */
-  void _getCgiEnvp(void);
+	/**
+	 *	@brief	Can be used to get the correct env for executing the CGI
+	 */
+	void _getCgiEnvp(void);
 };
 
 }; // namespace webserv
--- src/cgi.cpp
+++ src/cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:27:43 by adjoly            #+#    #+#             */
-/*   Updated: 2025/03/29 13:17:58 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/03/29 17:55:17 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 using namespace webserv;
 
 Cgi::Cgi(std::string &, std::string &, std::string &,
-         std::map<std::string, std::string> &, std::string &) {}
+		 std::map<std::string, std::string> &, std::string &) {}
 
 void Cgi::_getCgiEnvp(void) {
-  _cgi_envp.push_back("SERVER_SOFTWARE=webserv/" + std::string(WSRV_VERSION));
+	_cgi_envp.push_back("SERVER_SOFTWARE=webserv/" + std::string(WSRV_VERSION));
 
-  std::map<std::string, std::string>::iterator it = _http_header.find("Host");
-  if (it != _http_header.end())
-    _cgi_envp.push_back("SERVER_NAME" + it->second);
+	std::map<std::string, std::string>::iterator it = _http_header.find("Host");
+	if (it != _http_header.end())
+		_cgi_envp.push_back("SERVER_NAME=" + it->second);
+	else
+		_cgi_envp.push_back("SERVER_NAME=");
+	_cgi_envp.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	//_cgi_envp.push_back("SERVER_PORT=" + ""); - need to get port
 }
