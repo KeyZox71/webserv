cc2dc2396387aae412100e70c3841eba9c08a31d
adjoly <adamjly@proton.me>
2025-04-22 16:41:28 +0200

    「🔨」 fix: fixed some exception handling that were a little SUS

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:11:40 by adjoly            #+#    #+#             */
-/*   Updated: 2025/04/22 16:34:40 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/04/22 16:37:31 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,7 +149,7 @@ Server::Server(config::Config *conf) : _conf(conf) {
 	try {
 		_setup();
 		_run();
-	} catch (std::exception &e) {
+	} catch (std::runtime_error &e) {
 		_log->error(e.what());
 	}
 }
