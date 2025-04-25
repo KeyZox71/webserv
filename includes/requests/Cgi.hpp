1c694131cd4573b2929c399771f766d1f88341ad
Adam Joly <adjoly@2A7.42angouleme.fr>
2025-04-25 12:03:08 +0200

    「🏗️」 wip: Started cgi handling but no clue what i am doing

--- includes/requests/Cgi.hpp
+++ includes/requests/Cgi.hpp
@@ -6,31 +6,36 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 14:17:34 by adjoly            #+#    #+#             */
-/*   Updated: 2025/04/23 17:27:45 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/04/24 14:24:09 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "requests/HttpIMessage.hpp"
+#include "requests/HttpRequest.hpp"
+#include <config/default.hpp>
 #include <map>
 #include <string>
 
 namespace webserv {
 
-
 class Cgi {
-	public:
-		Cgi(http::IMessage &);
-		~Cgi(void);
+  public:
+	Cgi(http::IRequest *, config::Server *);
+	~Cgi(void);
 
-		void	initEnvp(void);
-		
-	protected:
-	private:
-		std::map<std::string, std::string> _envp;
+	std::string getEnv(std::string &);
+	void		setEnv(std::string &, std::string);
 
+	void	process(void);
 
-		std::string	_request;
+  protected:
+  private:
+	void _initEnvp(void);
+
+	std::map<std::string, std::string> _envp;
+	config::Server					  *_conf;
+	http::IMessage					  *_request;
 };
-};
+}; // namespace webserv
