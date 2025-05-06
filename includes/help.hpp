5ea07dd436d5525f6dee303b39b14801dc85c299
adjoly <adamjly@proton.me>
2025-05-06 17:10:25 +0200

    「✨」 feat: change to 0.2

--- includes/help.hpp
+++ includes/help.hpp
@@ -6,13 +6,13 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:43:54 by adjoly            #+#    #+#             */
-/*   Updated: 2025/04/26 16:14:37 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/05/06 17:10:16 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #define SAMPLE_CONF_PATH "./sample.toml"
-#define WEBSRV_VERSION "v0.1"
+#define WEBSRV_VERSION "v0.2"
 
 bool help(int, char **);
