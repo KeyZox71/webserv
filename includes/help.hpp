c87c4f26770ca437d0e3062f5b9158b1ea4d5ae8
adjoly <contact@adjoly.fr>
2025-07-11 17:14:20 +0200

    「✨」 feat: changed to 1.2

--- includes/help.hpp
+++ includes/help.hpp
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:43:54 by adjoly            #+#    #+#             */
-/*   Updated: 2025/05/15 12:02:30 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/07/11 17:06:11 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #else
 #define SAMPLE_CONF_PATH "/etc/webserv/default.conf"
 #endif
-#define WEBSRV_VERSION "v0.2"
+#define WEBSRV_VERSION "v1.1"
 #define WEBSRV_NAME "webserv"
 
 bool help(int, char **);
